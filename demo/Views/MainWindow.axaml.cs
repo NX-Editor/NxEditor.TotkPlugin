d354b6161169ca9e4e1ae65e58a5f4b243b4b04f
ArchLeaders <archleadership28@gmail.com>
2023-07-25 11:28:29 -0700

    Update demo

--- demo/Views/MainWindow.axaml.cs
+++ demo/Views/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using NxEditor.PluginBase.Models;
+using NxEditor.TotkPlugin.ViewModels;
 
 namespace NxEditor.TotkPlugin.Demo.Views;
 public partial class MainWindow : Window
@@ -6,5 +8,9 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        RestblEditorViewModel vm = new(new FileHandle("D:\\Bin\\RSTB\\totk\\ResourceSizeTable.Product.111.rsizetable"));
+        vm.Read();
+        Src.Content = vm.View;
     }
 }
