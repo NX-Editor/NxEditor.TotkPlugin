32ab993d133a25bf8d7b1e93d352b7e02d72ff2e
ArchLeaders <archleadership28@gmail.com>
2023-08-03 01:56:02 -0700

    Load 1.2.0 string table by default

--- src/TotkPlugin.cs
+++ src/TotkPlugin.cs
@@ -1,4 +1,6 @@
-﻿using NxEditor.PluginBase;
+﻿using ConfigFactory.Core;
+using ConfigFactory.Models;
+using NxEditor.PluginBase;
 
 namespace NxEditor.TotkPlugin;
 
@@ -8,6 +10,11 @@ public class TotkPlugin : IServiceExtension
 
     public void RegisterExtension(IServiceLoader serviceManager)
     {
+        if (Frontend.Locate<ConfigPageModel>().ConfigModules.TryGetValue("EpdConfig", out IConfigModule? module)) {
+            module.Properties["RestblStrings"].Property.SetValue(module,
+                Path.Combine(GlobalConfig.Shared.StorageFolder, "plugins", Name, "Resources", "Restbl", "string-table-1.2.0.txt"));
+        }
+            
         serviceManager
             .Register(new TotkZstd());
     }
