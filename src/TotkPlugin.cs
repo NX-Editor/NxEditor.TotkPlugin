eefe4e9175a0b4e2b136f5ce048c487ced996264
ArchLeaders <archleadership28@gmail.com>
2023-08-23 22:17:46 -0700

    Remove cs-restbl from registration

--- src/TotkPlugin.cs
+++ src/TotkPlugin.cs
@@ -1,6 +1,4 @@
-﻿using CsRestbl;
-using Native.IO.Services;
-using NxEditor.PluginBase;
+﻿using NxEditor.PluginBase;
 using NxEditor.PluginBase.Components;
 using NxEditor.TotkPlugin.Models;
 
@@ -13,12 +11,6 @@ public class TotkPlugin : IServiceExtension
 
     public void RegisterExtension(IServiceLoader serviceManager)
     {
-        NativeLibraryManager.RegisterAssembly(typeof(TotkPlugin).Assembly, out bool isCommonLoaded)
-            .Register(new RestblLibrary(), out bool isRestblLoaded);
-
-        Console.WriteLine($"Loaded native_io: {isCommonLoaded}");
-        Console.WriteLine($"Loaded cs_retbl: {isRestblLoaded}");
-
         TotkConfig.SetRestblStrings(TotkConfig.Shared.RestblGameVersion);
         Frontend.Locate<IMenuFactory>().Append(new TotkActionsMenu());
 
