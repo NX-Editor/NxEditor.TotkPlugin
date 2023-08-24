2f3d8f142f9dd2df3049faba184ad384034d2272
ArchLeaders <archleadership28@gmail.com>
2023-08-23 21:21:22 -0700

    Simplify & cleanup TotkActionsMenu

--- src/Models/TotkActionsMenu.cs
+++ src/Models/TotkActionsMenu.cs
@@ -14,20 +14,35 @@ public class TotkActionsMenu
     [Menu("Reset to Vanilla", "Totk/RSTB", icon: "fa-arrow-rotate-left")]
     public static async Task ResetToVanilla()
     {
-        DialogResult result = await DialogBox.ShowAsync("Warning", """
-             Changes made to the open RESTBL will be lost.
+        await CallActionMethod("ResetFromHandle", async () => {
+            return await DialogBox.ShowAsync("Warning", """
+                Changes made to the open RESTBL will be lost.
          
-             If you don't have one already, consider generating
-             a changeog (RCL) before resetting.
-             """, secondaryButtonContent: "Cancel");
+                If you don't have one already, consider generating
+                a changeog (RCL) before resetting.
+                """, secondaryButtonContent: "Cancel") == DialogResult.Primary;
+        });
+    }
 
-        if (result == DialogResult.Primary) {
-            IEditor? current = Frontend.Locate<IEditorManager>().Current;
-            if (current?.GetType() is Type type && type.Name == "RestblEditorViewModel") {
-                type.GetMethod("ResetFromHandle", BindingFlags.NonPublic | BindingFlags.Instance)?.Invoke(current, new object[] {
+    [Menu("Generate Changelog (RCL)", "Totk/RSTB", icon: "fa-wand-magic-sparkles", IsSeparator = true)]
+    public static async Task GenerateRCL()
+    {
+        await CallActionMethod("GenerateRclFromHandle");
+    }
+
+    private static async Task CallActionMethod(string methodName, Func<Task<bool>>? condition = null)
+    {
+        IEditor? current = Frontend.Locate<IEditorManager>().Current;
+        if (current?.GetType() is Type type && type.Name == "RestblEditorViewModel") {
+            if (condition == null || condition.Invoke() is Task<bool> task && await task == true) {
+                type.GetMethod(methodName, BindingFlags.NonPublic | BindingFlags.Instance)?.Invoke(current, new object[] {
                     new FileHandle(_vanillaRestblPath)
                 });
             }
+
+            return;
         }
+        
+        StatusModal.Set("Current document is not a restbl file", "fa-solid fa-triangle-exclamation", isWorkingStatus: false, temporaryStatusTime: 1.7);
     }
 }
